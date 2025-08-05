dd88701d1ddbdd128d0014051d28ae21ac2f251e
Logical Mind <sabitoj@gmail.com>
2025-08-05 02:22:06 -0400

    Update panel_B_bootstrap.py

--- src/shared/viscosity/panel_B_bootstrap.py
+++ src/shared/viscosity/panel_B_bootstrap.py
@@ -29,7 +29,7 @@ from sklearn.base import BaseEstimator, RegressorMixin
 from sdv.metadata import SingleTableMetadata
 from sdv.single_table import GaussianCopulaSynthesizer
 
-from model import DenseKANRBF, build_transformer_model
+from viscosity.model import DenseKANRBF, build_transformer_model
 
 # ------------------------------------------------------------------------------
 # 3) Regression Pipeline
